bee429f0151787afa8b9f6d1b2a15fda04efe538
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-23 11:54:10 -0700

    test file

--- test/0_test.c
+++ test/0_test.c
@@ -7,13 +7,62 @@
  * Return: 0 (success)
  *
  */
-int main (void)
+int main(void)
 {
-	int c1;
-	int c2;
 
-	c1 = _printf("Test Passed.\n");
-	c2 = printf("Test Passed.\n");
+	_printf("Test Passed.\n");
+	printf("Test Passed.\n");
 
 	return (0);
+}
+
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _printf - function that produces output according to a format.
+ *
+ * @format: input character string.
+ *
+ * Return: the number of characters printed.
+ *
+ */
+int _printf(const char *format, ...)
+{
+  int i = 0;
+  int len = 0;
+  va_list args;
+
+  va_start(args, format);
+
+  while (format != NULL && format [i] != '\0')
+    {
+      switch (format[i])
+	{
+	case 'c':
+	  _putchar(va_arg(args, int));
+	  break;
+
+	default:
+	  break;
+	}
+      i++;
+    }
+  va_end(args);
+  return (len);
+}
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
 }
