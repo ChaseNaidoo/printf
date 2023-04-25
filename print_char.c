348df9da6db62eef3a80fc547ab6ee3a921367b5
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-25 13:45:43 -0700

    Fix Betty style

--- print_char.c
+++ print_char.c
@@ -3,8 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * print_char - prints a single character to standard output
+ *
+ * @args: variable argument list
+ *
+ * Return: the number of characters written
+ *
+ */
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
-	return write(1, &c, 1);
+
+	return (write(1, &c, 1));
 }
