e4aaec74da9c82fa896129d589edc9a58bff8606
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-25 07:07:44 -0700

    added test files

--- test/_printf.c
+++ test/_printf.c
@@ -1,40 +1,83 @@
-#include "main.h"
-#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
- * _printf - function that produces output according to a format.
+ * _printf - prints output according to a format
  *
- * @format: input character string.
+ * @format: the format string
  *
- * Return: the number of characters printed.
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  *
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int len = 0;
+	int count;
 	va_list args;
-
 	va_start(args, format);
 
-	while (format != NULL && format [i] != '\0')
+	count = 0;
+
+	while (*format)
 	{
-	if (format[i] == '%')
-	format++;
-	  
-	switch (format[i])
+	if (*format == '%')
+	{
+		format++;
+
+		switch (*format)
 		{
 		case 'c':
-		_putchar(va_arg(args, int));
-		len++;
-		break;
-
-		default:
+		{
+		char c = va_arg(args, int);
+		if (write(1, &c, 1) == -1)
+		return (-1);
+		count++;
 		break;
 		}
-		i++;
+
+		case 's':
+		{
+		char *str = va_arg(args, char*);
+		while (*str)
+		{
+		if (write(1, str, 1) == -1)
+		return (-1);
+		str++;
+		count++;
+		}
+		break;
+		}
+
+		case '%':
+		{
+		if (write(1, "%", 1) == -1)
+		return (-1);
+		count++;
+		break;
+		}
+
+		default:
+		{
+		if (write(1, "%", 1) == -1)
+		return (-1);
+		count++;
+		format--;
+		break;
+		}
+		}
 	}
+	else
+	{
+	if (write(1, format, 1) == -1)
+	return (-1);
+	count++;
+	}
+	format++;
+	}
+
 	va_end(args);
-	return (len);
+
+	return (count);
 }
