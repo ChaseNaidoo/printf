b7f8939f0f47f27c5e2cc645540a15d7ff71e213
ChaseNaidoo <chasenaidoo9@gmail.coom>
2023-04-25 07:00:54 -0700

    changed code to match fork

--- _printf.c
+++ _printf.c
@@ -6,46 +6,78 @@
 
 /**
  * _printf - prints output according to a format
+ *
  * @format: the format string
  *
  * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ *
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	char buffer[1024];
-	int (*handler)(va_list, char*, unsigned int);
+	int count;
 	va_list args;
-
 	va_start(args, format);
+
+	count = 0;
 
 	while (*format)
 	{
-      if (*format == '%')
+	if (*format == '%')
 	{
-	  format++;
-	handler = print_func(format);
-	  if (handler == NULL)
-	    {
-	      if (write(1, "%", 1) == -1)
+		format++;
+
+		switch (*format)
+		{
+		case 'c':
+		{
+		char c = va_arg(args, int);
+		if (write(1, &c, 1) == -1)
 		return (-1);
-	      count++;
-	    }
-	  else
-	    {
-	      format = handle_specifier(format, handler, args, buffer, &count);
-	    }
+		count++;
+		break;
+		}
+
+		case 's':
+		{
+		char *str = va_arg(args, char*);
+		while (*str)
+		{
+		if (write(1, str, 1) == -1)
+		return (-1);
+		str++;
+		count++;
+		}
+		break;
+		}
+
+		case '%':
+		{
+		if (write(1, "%", 1) == -1)
+		return (-1);
+		count++;
+		break;
+		}
+
+		default:
+		{
+		if (write(1, "%", 1) == -1)
+		return (-1);
+		count++;
+		format--;
+		break;
+		}
+		}
 	}
-      else
+	else
 	{
-	  if (write(1, format, 1) == -1)
-	    return (-1);
-	  count++;
+	if (write(1, format, 1) == -1)
+	return (-1);
+	count++;
 	}
-	  format++;
-    }
+	format++;
+	}
 
-  va_end(args);
+	va_end(args);
 
-  return (count);
+	return (count);
 }
--- main.h
+++ main.h
@@ -4,25 +4,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-/**
- * struct print - Struct for print functions
- *
- * @format_specifier: string representing the format specifier
- * @handler: function pointer to the correct print function for the format specifier.
- *
- */
-typedef struct print
-{
-	char *format_char;
-	int (*handler)(va_list, char *, unsigned int);
-}	print_t;
-
 int _putchar(char c);
 int _printf(const char *format, ...);
-int (*print_func(const char *s))(va_list, char *, unsigned int);
-const char *handle_specifier(const char *format, int (*handler)(va_list, char *, unsigned int), va_list args, char *buffer, int *count);
-int print_char(va_list args, char *buffer, unsigned int buffer_index);
-int print_str(va_list args, char *buffer, unsigned int buffer_index);
-int print_pct(va_list args __attribute__((unused)), char *buffer, unsigned int buffer_index);
 
 #endif
