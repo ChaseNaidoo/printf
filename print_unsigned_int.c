6d295e438bbe89a7161fa60e65a4d21fc7ba6dcf
Romano1898 <romano.naidoo@gmail.com>
2023-04-26 02:14:25 +0200

    Task3

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -10,22 +10,19 @@
  * @args: arguments passed to the function
  * Return: the number of characters printed
  */
-int print_unsigned(va_list args)
+int print_unsigned_int(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
-	unsigned int divisor = 1, remainder = 0;
+	unsigned int divisor = 1;
 	int digit = 0, count = 0;
 
-	/* Determine the divisor */
 	while (num / divisor > 9)
 	{
 		divisor *= 10;
 	}
-
-	/* Print the number */
+      
 	while (divisor != 0)
 	{
-		remainder = num % divisor;
 		digit = (num / divisor) % 10;
 		count += _putchar(digit + '0');
 		divisor /= 10;
